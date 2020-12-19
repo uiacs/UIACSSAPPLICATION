b5eb9c29734a8eac6f2c8f6ed20d0ec005a6f6dc
uiacs <uiacs.project@gmail.com>
2020-12-19 14:02:59 +0700

    initial commit

---
+++ app/src/main/java/com/example/uiacssplash/detailnotes.java
@@ -0,0 +1,14 @@
+package com.example.uiacssplash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detailnotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailnotes);
+    }
+}
